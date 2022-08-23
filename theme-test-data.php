8047d58fcf82151a44f831a83fe408bf028fa4b1
DanhThong <danhthong@gmail.com>
2022-08-23 16:18:59 +0700

    feat: add plugin file

---
+++ theme-test-data.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Theme Unit Test
+ * Plugin URI: https://github.com/abstractwp/theme-test-data
+ * Description: The theme unit test data for demo.
+ * Author: AbstractWP
+ * Author URI: https://www.abstractwp.com/
+ * Tags: theme test data, unit test, test
+ * Version: 1.0.0
+ * Text Domain: 'theme-unit-test'
+ * Domain Path: languages
+ * Tested up to: 6.0.1
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Theme Unit Test. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package   Theme Unit Test
+ * @author    AbstractWP
+ * @license   GPL-3.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
